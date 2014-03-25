bc274f84af05e0a1e029a2fd286273239d88a811
Cherub Kumar <cherub.kumar@gmail.com>
2014-03-25 04:13:22 -0500

    Add Recipe-User Relationship and Indices on Foreign Keys -recipes has_many users -users_has_many_recipes =- *corrected migration

--- db/migrate/20140325085504_add_user_id_to_recipes.rb
+++ db/migrate/20140325085504_add_user_id_to_recipes.rb
@@ -1,4 +1,4 @@
-class AddUserIdToRecipies < ActiveRecord::Migration
+class AddUserIdToRecipes < ActiveRecord::Migration
   def change
     add_column :recipes, :user_id, :integer
     add_index :recipes, :user_id
