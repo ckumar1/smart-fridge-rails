29ea0f6e865c550250d745138920a9df91868b0b
Kostykins <kostelecky@wisc.edu>
2014-03-27 13:41:27 -0500

    Added Food and Recipe Pages and Fixed navbar
    - Added Food Page
    - Added Recipe Page
    - Fixed navbar so that the correct link is “highlighted”
      so the user knows which page they are on.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ SmartFridgeRails::Application.routes.draw do
 
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/food"
+  get "static_pages/recipes"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
