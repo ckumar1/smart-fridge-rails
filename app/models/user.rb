3d775cbcc0ec4735b672e81463a4220d3e34be13
Kostykins <kostelecky@wisc.edu>
2014-04-06 20:39:59 -0500

    added name regex
    - created regular expression for the name field, just in case someone
    tries to do some evil name creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   include Clearance::User
   before_save { self.email = email.downcase }
   # Validations
-  validates :name, presence: true, length: { maximum: 50 }
+  VALID_NAME_REGEX = /\A[a-z0-9_-]{3,16}\Z/
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
 
  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
  # validates :email, format: { with: VALID_EMAIL_REGEX }
