c06ce118c97bb90ad5cd957c7bf5dcbed7d75aac
Kostykins <kostelecky@wisc.edu>
2014-04-10 23:51:11 -0500

    - Commented out food item!
    - IF FOOD ITEM is commented out of user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   VALID_NAME_REGEX = /\A([a-zA-Z0-9_-]|(\s)?){3,16}\Z/
 
   #make sure not a food item, as both User and FoodItem have name fields
-  if !FoodItem
-    validates :name, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
-  end
+  #if !FoodItem
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
+ # end
  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
  # validates :email, format: { with: VALID_EMAIL_REGEX }
 
