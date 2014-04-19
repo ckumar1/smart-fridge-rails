9aed8c87ef64dad4d061bc51b4288c80f69232db
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-18 23:41:48 -0500

    Add associations to factory

--- spec/factories/Factories.rb
+++ spec/factories/Factories.rb
@@ -6,12 +6,16 @@ FactoryGirl.define do
     f.name { Faker::Name.name }
     f.email { Faker::Internet.email }
     f.password { Faker::Internet.password(8) }
+    association :food_item
   end
 
 
   factory :food_item do |i|
     i.name "Apple"
     i.expiration_date { 2.weeks.from_now }
-    i.user_id {}
+    i.calories 80
+    i.description 'fresh and red'
+    i.description 'fresh and red'
+    association :user
   end
 end
