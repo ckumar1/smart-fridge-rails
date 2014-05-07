3390eeb8eff0caf380781687842e8c6d1bb90c1c
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-06 23:50:28 -0500

    Update API sign-in tests
    -Change before :all to :each
    -Added tests for content of user array

--- spec/requests/api_login_spec.rb
+++ spec/requests/api_login_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe 'API sign-in endpoint' do
+describe 'API Sign-In Endpoint' do
 
   let(:valid_user) { create(:user) }
   let(:hostname) { 'http://api.smartfridge.com' }
 
   context 'for valid credentials' do
-    before(:all) do
+    before(:each) do
       post "#{hostname}/access", {email: valid_user.email, password: valid_user.password}.to_json,
            { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
     end
@@ -21,13 +21,17 @@ describe 'API sign-in endpoint' do
 
     context 'response body' do
       subject(:access_response) { JSON.parse(response.body, symbolize_names: true) }
+      let (:authorized_user) { {uid: valid_user.id, email: valid_user.email, name: valid_user.name}.to_json }
 
       its ([:success]) { should eq(1) }
+      its ([:user]) { should have(1).obj }
+      its ([:user]) { should include(authorized_user)}
+
     end
   end
 
   context 'for invalid credentials' do
-    before(:all) do
+    before(:each) do
       post "#{hostname}/access", {email: valid_user.email, password: ''}.to_json,
            { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
     end
