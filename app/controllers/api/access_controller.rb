f0d78c58bca2b2ad8e3fb6fabec556eac0464e57
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-06 23:48:47 -0500

    Implement Sign-in API
    Endpoint: POST api.<domain>/access
    
    // Request
    {
    	"email": "user@email.com",
    	"password": "password123"
    }
    
    // Response
    {
      "success": 1,
      "user": [{
        "uid": 1,
        "email": "user@email.com",
        "name": "sample user"
      }]
    }

--- app/controllers/api/access_controller.rb
+++ app/controllers/api/access_controller.rb
@@ -1,7 +1,22 @@
 module API
   class AccessController < ApplicationController
 
+    respond_to :json
+
+
     def authenticate
+      @user = User.find_by(email: params[:email].downcase)
+
+      if @user && User.authenticate(params[:email].downcase, params[:password])
+
+        found_user = {uid: @user.id, email: @user.email, name: @user.name}.to_json
+
+        render json: {success: 1, user: [found_user]}, status: :ok, location: @user
+       
+      else
+        render json: {error: "Invalid Credentials"}, status: :unprocessable_entity
+
+      end
 
     end
 
