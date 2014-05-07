6fb0e5ca04ec84d141fb70cd6e844fa68de7c8b2
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-06 21:43:24 -0500

    Add tests for /access api endpoint
    - tests for both valid and invalid credentials

--- spec/requests/api_login_spec.rb
+++ spec/requests/api_login_spec.rb
@@ -7,18 +7,35 @@ describe 'API sign-in endpoint' do
 
   context 'for valid credentials' do
     before(:all) do
-      post "#{hostname}/access", {email: valid_user.email, password: valid_user.password}
+      post "#{hostname}/access", {email: valid_user.email, password: valid_user.password}.to_json,
+           { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
     end
     subject { response }
 
-    its ([:status]) { should eq(200) }
-    its ([:body]) { should_not be_empty }
+    its (:status) { should eq(200) }
+    its (:body) { should_not be_nil }
+    its (:content_type) { should eq(Mime::JSON)}
+
+
+    its (:location) { should eq( user_url(valid_user.id ) )}
 
     context 'response body' do
-      subject { response.body }
+      subject(:access_response) { JSON.parse(response.body, symbolize_names: true) }
 
       its ([:success]) { should eq(1) }
     end
+  end
 
+  context 'for invalid credentials' do
+    before(:all) do
+      post "#{hostname}/access", {email: valid_user.email, password: ''}.to_json,
+           { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
+    end
+
+    subject { response }
+
+    its (:status) { should eq(422) }
+    its (:body) { should_not be_nil }
+    its (:content_type) { should eq(Mime::JSON)}
   end
 end
