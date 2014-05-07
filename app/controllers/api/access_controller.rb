4ff8c16e6ff2a7eb9cda0274893690c43589d115
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-07 02:43:11 -0500

    Remove signup action from access controller

--- app/controllers/api/access_controller.rb
+++ app/controllers/api/access_controller.rb
@@ -3,25 +3,18 @@ module API
 
     respond_to :json
 
-
     def authenticate
       @user = User.find_by(email: params[:email].downcase)
-
       if @user && User.authenticate(params[:email].downcase, params[:password])
-
-        found_user = {uid: @user.id, email: @user.email, name: @user.name}.to_json
-
-        render json: {success: 1, user: [found_user]}, status: :ok, location: @user
-       
+        found_user = {uid: @user.id, email: @user.email,
+                      name: @user.name}.to_json
+        render json: {success: 1, user: [found_user]},
+               status: :ok, location: @user
       else
-        render json: {error: "Invalid Credentials"}, status: :unprocessable_entity
-
+        render json: {error: "Invalid Credentials"},
+               status: :unprocessable_entity
       end
-
     end
 
-    def signup
-
-    end
   end
 end
