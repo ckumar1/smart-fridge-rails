39212426ed188393ca4194a25f00a29c6892e60e
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-08 11:39:43 -0500

    Validate presence of recipe in ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,11 @@
 class Ingredient < ActiveRecord::Base
 
+			
+
   # Validations
   validates :name, presence: true, length: {maximum: 50}
   validates :quantity, presence: true
+  validates :recipe, presence: true
 
   # Relationships
   belongs_to :recipe
