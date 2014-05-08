c71668ed649a1b3e1eaef2d45ccd90757f9eb994
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-08 06:22:49 -0500

    Fix Recipe Factory to return text instead of arrays - Name and Direction were affected fields - Caused by unchecked return value of Faker Gem methods

--- spec/factories/Factories.rb
+++ spec/factories/Factories.rb
@@ -2,27 +2,25 @@ require 'faker'
 
 FactoryGirl.define do
 
-  factory :user do |f|
-    f.name { Faker::Name.name }
-    f.email { Faker::Internet.email }
-    f.password { Faker::Internet.password(8) }
+  factory :user do |u|
+    u.name { Faker::Name.name }
+    u.email { Faker::Internet.email }
+    u.password { Faker::Internet.password(8) }
   end
 
-
-  factory :food_item do |i|
-    i.name { Faker::Lorem.words 1 }
-    i.expiration_date { 2.weeks.from_now }
-    i.calories { rand(500) }
-    i.description { Faker::Lorem.sentence }
-    association :user
+  factory :food_item do |f|
+    f.name { Faker::Lorem.words 1 }
+    f.expiration_date { 2.weeks.from_now }
+    f.calories { rand(500) }
+    f.description { Faker::Lorem.sentence }
+    user
   end
 
-  factory :recipe do |i|
-    i.name { Faker::Lorem.words 2 }
-    i.directions { Faker::Lorem.sentences 5 }
-    i.ingredients { Faker::Lorem.words 10 }
-    i.notes { Faker::Lorem.paragraph }
-    association :user
+  factory :recipe do |r|
+    r.name { Faker::Lorem.words(2).join(' ') }
+    r.directions { Faker::Lorem.sentences(5).join(' ') }
+    r.notes { Faker::Lorem.paragraph }
+    user
   end
 
 end
