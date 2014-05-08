477d01dd98408ca249ce08c0fea140ead951979f
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-08 06:24:21 -0500

    Implement ingredient model tests - create ingredient factory

--- spec/factories/ingredients.rb
+++ spec/factories/ingredients.rb
@@ -1,9 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :ingredient do
-    name "Diced Tomatos"
-    quantity "2 Cups"
-    association :recipe
+    name 'Diced Tomatos'
+    quantity '2 Cups'
+    recipe
   end
+
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Ingredient do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let (:rcp) { create :recipe }
+
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:ingredient, recipe: rcp)).to be_valid
+  end
+
+  context 'is valid' do
+    let (:valid_ingredient) { build(:ingredient) }
+
+    it 'with a name and quantity' do
+      expect(valid_ingredient).to be_valid
+    end
+  end
+
+  context 'is invalid' do
+    let (:ingredient_without_name) { build :ingredient, name: '' }
+    let (:ingredient_without_quantity) { build :ingredient, quantity: '' }
+    let (:lengthy_ingredient) { build(:ingredient, name: 'a'*51) }
+
+    it 'without a name' do
+      expect(ingredient_without_name).to_not be_valid
+    end
+    it 'without a quantity' do
+      expect(ingredient_without_quantity).to_not be_valid
+    end
+    it 'if name is longer than 50 characters' do
+      expect(lengthy_ingredient).to_not be_valid
+    end
+  end
+
 end
