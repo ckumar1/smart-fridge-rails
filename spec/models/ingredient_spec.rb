3e6b34cc3509785690cf4b2aa5a50b41c6816580
Cherub Kumar <cherub.kumar@gmail.com>
2014-05-08 11:39:14 -0500

    Test ingredients for the presence of a recipe
    - Refactor valid factory test
    - Add presence validation to ingredient to make test PASS

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe Ingredient do
-
-  let (:rcp) { create :recipe }
-
-  it "has a valid factory" do
-    expect(FactoryGirl.build(:ingredient, recipe: rcp)).to be_valid
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:ingredient)).to be_valid
   end
 
   context 'is valid' do
@@ -14,12 +11,14 @@ describe Ingredient do
     it 'with a name and quantity' do
       expect(valid_ingredient).to be_valid
     end
+
   end
 
   context 'is invalid' do
     let (:ingredient_without_name) { build :ingredient, name: '' }
     let (:ingredient_without_quantity) { build :ingredient, quantity: '' }
     let (:lengthy_ingredient) { build(:ingredient, name: 'a'*51) }
+    let (:unassociated_ingredient) { build :ingredient, recipe: nil }
 
     it 'without a name' do
       expect(ingredient_without_name).to_not be_valid
@@ -27,6 +26,11 @@ describe Ingredient do
     it 'without a quantity' do
       expect(ingredient_without_quantity).to_not be_valid
     end
+
+    it 'without an associated recipe' do
+      expect(unassociated_ingredient).to_not be_valid
+    end
+
     it 'if name is longer than 50 characters' do
       expect(lengthy_ingredient).to_not be_valid
     end
